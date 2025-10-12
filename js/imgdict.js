2b3c6db4b7fdff9a7d8eb3d66a525a0a26a2dd6a
richmantyler73 <richmantyler73@gmail.com>
2025-10-11 22:44:44 -0400

    Reformat Repository

--- js/imgdict.js
+++ js/imgdict.js
@@ -1,6 +1,6 @@
 const imgdict = {
     main: [
-        'jpg/main/test0.JPG',
+        'jpg/title/test0.JPG',
         'jpg/main/test1.JPG',
         'jpg/main/test2.JPG',
         'jpg/main/test3.JPG'
